d2ec4001348c318916fbdfbf6fe0e8e4927fd4c1
Danila <Danila@DESKTOP-9FOCATO>
2023-04-04 01:06:24 +0300

    Добавьте файлы проекта.

---
+++ StudentApp/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace StudentApp
+{
+    [SQLite.Table("Students")]
+    public class Student
+    {
+        [PrimaryKey, AutoIncrement, SQLite.Column("_id")]
+        public int Id { get; set; }
+        public string Group { get; set; }
+        public string FIO { get; set; }
+    }
+}
